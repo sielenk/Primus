3d3851cd89e85f3180c8f7efaabb3df123db5c38
Marvin H. Sielenkemper <sielenk@gmx.de>
2017-02-10 21:52:41 +0100

    some automatic updates

--- app/src/main/java/de/masitec/primus/MainActivity.java
+++ app/src/main/java/de/masitec/primus/MainActivity.java
@@ -1,13 +1,13 @@
 package de.masitec.primus;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+    public final static String EXTRA_MESSAGE = "de.masitec.primus.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
